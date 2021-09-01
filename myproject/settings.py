5f5070f3e7a96252b6edf584bedfa48e3cb33728
Kyomu-Rin <rinawkmbi4k8@gmail.com>
2021-09-02 01:11:52 +0900

    修正

--- myproject/settings.py
+++ myproject/settings.py
@@ -137,7 +137,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FOWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'myapp/static'),)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
