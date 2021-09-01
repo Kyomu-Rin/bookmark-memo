1528b9c25ef55d5e61ad0f23dab5b10e879c2d1b
Kyomu-Rin <rinawkmbi4k8@gmail.com>
2021-09-02 01:57:52 +0900

    static

--- myproject/settings.py
+++ myproject/settings.py
@@ -134,7 +134,7 @@ ALLOWED_HOSTS = ['*']
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'myapp/static')
 
 DEBUG = False
 
